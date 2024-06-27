9116156fc55f18678c028df33b7c4d3ea3ebd4c6
eclipse-consulting <short.gamma@icloud.com>
2024-06-27 00:15:35 -0400

    ci

---
+++ src/lib/sand2008.cpp
@@ -0,0 +1,5 @@
+#include "sand2008/sand2008.h"
+
+int compute_m1() {
+    return 42;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
-int main()
-{
+#include <iostream>
+#include "sand2008/sand2008.h"
+
+int main() {
+    std::cout << "compute_m1 function returns: " << compute_m1() << std::endl;
     return 0;
 }
